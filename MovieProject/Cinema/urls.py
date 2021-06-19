3978f542416f3fd633012c0bbe0d1ddbc4cde773
bzhumakova <bzhumakova@gmail.com>
2021-06-20 01:51:05 +0600

    register path for movies_page,movie_detail_page,genres_page,genres_movies

---
+++ MovieProject/Cinema/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import *
+
+urlpatterns=[
+     path('movies/',movies_page,name='movies'),
+     path('movie_detail/<int:movie_id>/',movie_detail_page,name='movie_detail'),
+     path('',genres_page,name='genres'),
+     path('genre_movies/<int:genre_id>/',genre_movies,name='genre_movies'),
+]
